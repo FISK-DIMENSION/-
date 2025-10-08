f5befb28da02fcc819c1894bf89e8dcca5963959
King Fisk AKA M.C OTC <125013959+OTC15203@users.noreply.github.com>
2025-10-08 05:14:51 -0500

    Revert "Add Fisk Dimension DAO disclosure module"

--- fisk-dimension/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import clsx from "clsx";
-
-export function Card({ className, ...props }) {
-  return <div className={clsx("rounded-xl border bg-white/5 text-white shadow", className)} {...props} />;
-}
-
-export function CardContent({ className, ...props }) {
-  return <div className={clsx("p-6", className)} {...props} />;
-}
